f91b4b1d7acbf46711a432e85aebb02e54062167
Julia Anisio <julia.an.nogueira@gmail.com>
2026-02-03 13:53:22 -0300

    domain model user role, seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -23,6 +21,13 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
     //relacionamento
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
@@ -36,6 +41,19 @@ public class User {
         this.phone = phone;
         this.birthDate = birthDate;
         this.password = password;
+    }
+
+    public void addRole(Role role){
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName){
+        for(Role role : roles){
+            if(role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public Long getId() {
@@ -84,6 +102,10 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 
     public List<Order> getOrders() {
